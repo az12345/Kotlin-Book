f55bb9e121487141da89e1a6b5507a1e296ba659
spdeepak <speedpak1991@gmail.com>
2017-12-28 17:35:46 +0100

    If statement tutorial added

---
+++ src/main/kotlin/com/github/spdeepak/chapter3/IfExpression.kt
@@ -0,0 +1,23 @@
+package com.github.spdeepak.chapter3
+
+fun main(args: Array<String>) {
+	var max = 1
+	println("value of max: $max")
+	if (1 < 2) max = 2
+	println("value of max: $max")
+
+	val min = if (1 < 2) {
+		1
+	} else {
+		2
+	}
+	println("Value of min: $min")
+
+	val a = 100
+	val b = 101
+	if (a > b) {
+		println("a > b")
+	} else {
+		println("a < b")
+	}
+}
